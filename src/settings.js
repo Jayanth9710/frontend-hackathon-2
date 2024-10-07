3938e2e730c624797a652557fd56a01ee237d287
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2024-10-07 22:10:56 +0530

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,8 @@
 let getSettings = () => {
     return {
         // api : "http://localhost:3000"
-        api : "https://hackathon-backend-2.herokuapp.com"
+        // api : "https://hackathon-backend-2.herokuapp.com"
+        api : "https://web-scraping-ten.vercel.app"
     }
 }
 
